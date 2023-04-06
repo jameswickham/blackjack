ff2641e3a12d09ea9fd2e8d02704d3b275ab707a
James Wickham <james.wickham@proagrica.com>
2023-04-06 16:37:07 +0100

    adjusting formatting

--- src/main/java/com/james/Game.java
+++ src/main/java/com/james/Game.java
@@ -48,15 +48,12 @@ public class Game {
     System.out.println("The game of Blackjack");
     System.out.println("");
     System.out.println("  BLACKJACK RULES: ");
-    System.out
-        .println("    -Each player is dealt 2 cards. The dealer is dealt 2 cards with one face-up and one face-down.");
+    System.out.println("    -Each player is dealt 2 cards. The dealer is dealt 2 cards with one face-up and one face-down.");
     System.out.println("    -Cards are equal to their value with face cards being 10 and an Ace being 1 or 11.");
     System.out.println("    -The players cards are added up for their total.");
-    System.out.println(
-        "    -Players “Hit” to get another card from the deck. Players “Stick” if they wish to keep their current card total.");
+    System.out.println("    -Players “Hit” to get another card from the deck. Players “Stick” if they wish to keep their current card total.");
     System.out.println("    -Dealer “Hits” until they equal or exceed 17.");
-    System.out
-        .println("    -The goal is to have a higher card total than the dealer without exceeding 21(going bust!).");
+    System.out.println("    -The goal is to have a higher card total than the dealer without exceeding 21(going bust!).");
     System.out.println("    -Players win if they beat the dealer");
     System.out.println("");
   }
